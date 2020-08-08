be111488a42735e185d6e642b28cdd5111d7e34f
coder-shahjalal <shahjalal.freelancer@gmail.com>
2020-08-09 02:13:10 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//Falsy:
+//false
+//0
+//""
+//udenfined
+//null
+ //Nan
+ //Truthy:
+ //'0'," " []
+if(){
+    console.log("condition is true");
+
+}
+
+else {
+    console.log("condition is false");
+}
